aac9918cfb53f7f916e3bf971a5ee04a5e2b9837
jalorenzol <lorenzollanesj@gmail.com>
2022-11-02 02:12:59 -0400

    Primera app Rect con el uso de states

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,16 +1,35 @@
+import PropTypes from "prop-types"
 import React from 'react'
 
-const PrimeraApp = () => {
-    const saludo = {
+
+
+const PrimeraApp = ({saludo, texto}) => {
+    const personal = {
         nombre: "Jose",
         edad: 29
     }
 
+    
+
     return (
-        <><h1>{saludo.nombre}</h1>
-            <p>Mi primera aplicacion</p></>
+        <><h1>{saludo}</h1>
+            <p>Mi primera aplicacion de {personal.nombre}</p>
+            <p>{texto}</p>
+            
+            </>
+
 
     );
 }
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    texto: 'Contenido'
+}
+
+
 
 export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+// import PrimeraApp from './PrimeraApp';
 
 
 const divRoot = document.querySelector('#root')
 
-ReactDOM.render(<PrimeraApp />,divRoot) 
+ReactDOM.render(<CounterApp value={0}/>,divRoot) 
+
